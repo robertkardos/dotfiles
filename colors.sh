6c0f7fe207bf2ff0510930433a0a737d60a018d6
Robert Kardos <kardos.robert.kh@gmail.com>
2025-04-30 22:14:52 +0200

    Add new colors.sh

--- colors.sh
+++ colors.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+#
+#   This file echoes a bunch of color codes to the 
+#   terminal to demonstrate what's available.  Each 
+#   line is the color code of one forground color,
+#   out of 17 (default + 16 escapes), followed by a 
+#   test use of that color on all nine background 
+#   colors (default + 8 escapes).
+#
  
-# This program is free software. It comes without any warranty, to
-# the extent permitted by applicable law. You can redistribute it
-# and/or modify it under the terms of the Do What The Fuck You Want
-# To Public License, Version 2, as published by Sam Hocevar. See
-# http://sam.zoy.org/wtfpl/COPYING for more details.
+T='gYw'   # The test text
  
-for fgbg in 38 48 ; do #Foreground/Background
-	for color in {0..256} ; do #Colors
-		#Display the color
-		echo -en "\e[${fgbg};5;${color}m col${color}\t\e[0m"
-		#Display 8 colors per lines
-		if [ $((($color + 1) % 8)) == 0 ] ; then
-			echo #New line
-		fi
-	done
-	echo #New line
+echo -e "\n                 40m     41m     42m     43m\
+     44m     45m     46m     47m";
+ 
+for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
+           '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
+           '  36m' '1;36m' '  37m' '1;37m';
+  do FG=${FGs// /}
+  echo -en " $FGs \033[$FG  $T  "
+  for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+    do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
+  done
+  echo;
 done
-exit 0
+echo
