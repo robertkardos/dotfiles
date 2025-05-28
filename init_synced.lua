27d539522ec1bb75b5d62ac8b43ba2ad23e20b80
Robert Kardos <kardos.robert.kh@gmail.com>
2025-05-28 03:22:56 +0200

    Add NeoVim config support

--- init_synced.lua
+++ init_synced.lua
@@ -1,1 +1,12 @@
+vim.cmd([[
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc
+]])
+
+print(runtimepath)
+-- require("config.lazy")
+
 print("synced shaite loaded")
+
+
