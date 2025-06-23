273e6e0aa7f090d607eefa0563c43141498816c4
Robert Kardos <kardos.robert.kh@gmail.com>
2025-06-23 13:28:22 +0200

    Add Neovim Lualine

--- nvim/lua/init_synced.lua
+++ nvim/lua/init_synced.lua
@@ -100,6 +100,75 @@ vim.keymap.set('n', '<space><tab>', ':Neotree toggle current reveal_force_cwd<cr
 vim.keymap.set("n", "<leader>F", ":lua vim.lsp.buf.format()<CR>", { desc = "auto format" })
 vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>", { desc = "show diagnostics" })
 
+
+
+
+
+
+
+
+
+require('lualine').setup({
+	--options = { theme = 'night-owl' }
+	options = {
+		icons_enabled = true,
+		theme = 'auto',
+		component_separators = { left = ' ', right = ' ' },
+		section_separators = { left = '', right = '' },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		always_show_tabline = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+			refresh_time = 16, -- ~60fps
+			events = {
+				'WinEnter',
+				'BufEnter',
+				'BufWritePost',
+				'SessionLoadPost',
+				'FileChangedShellPost',
+				'VimResized',
+				'Filetype',
+				'CursorMoved',
+				'CursorMovedI',
+				'ModeChanged',
+			},
+		}
+	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diff', 'diagnostics' },
+		lualine_c = { 'filename' },
+		lualine_x = { 'encoding', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' }
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { 'filename' },
+		lualine_x = { 'location' },
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {}
+
+})
+
+
+
+
+
 vim.cmd.colorscheme "night-owl"
 -- vim.cmd.colorscheme "everblush"
 
