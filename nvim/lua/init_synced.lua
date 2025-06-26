f68fbd5932993f1a4b74ad378161dde0cb8308a4
Robert Kardos <kardos.robert.kh@gmail.com>
2025-06-26 23:13:51 +0200

    NVIM: Show path to file on Lualine

--- nvim/lua/init_synced.lua
+++ nvim/lua/init_synced.lua
@@ -159,7 +159,7 @@ require('lualine').setup({
 	sections = {
 		lualine_a = { 'mode' },
 		lualine_b = { 'branch', 'diff', 'diagnostics' },
-		lualine_c = { 'filename' },
+		lualine_c = { {'filename', path = 1} },
 		lualine_x = { 'encoding', 'filetype' },
 		lualine_y = { 'progress' },
 		lualine_z = { 'location' }
