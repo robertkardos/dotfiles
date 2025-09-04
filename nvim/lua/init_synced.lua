10146e8f8e90d092a90603d9fefe9ebf98f27a92
Robert Kardos <robert.kardos@ericsson.com>
2025-09-04 12:40:17 +0200

    Disable irritating Lazy notifications
    Change-Id: Ic3644ace5a8d573bc29fe9ae9df15653a3dfbc03

--- nvim/lua/init_synced.lua
+++ nvim/lua/init_synced.lua
@@ -39,7 +39,7 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	--	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = { enabled = true, notify = false },
 })
 
 
