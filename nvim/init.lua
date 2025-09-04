502041ee216641034f895b701a69ed9cc5ef60a8
Robert Kardos <robert.kardos@ericsson.com>
2025-09-04 12:35:42 +0200

    Configure tabs for Neovim
    Change-Id: I844c39ee6a73e02c3bbd35db231415236d3cf2a8

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,8 @@
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
 require("init_synced")
+
+vim.opt["tabstop"] = 4
+vim.opt["softtabstop"] = 4
+vim.opt["shiftwidth"] = 4
+vim.opt["expandtab"] = true
