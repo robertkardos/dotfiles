dea401037b151dd21df89ba170fafa974bb930c9
Robert Kardos <kardos.robert.kh@gmail.com>
2026-05-15 20:09:42 +0200

    N/VIM: Add Gitsigns bindings

--- nvim/lua/init_synced.lua
+++ nvim/lua/init_synced.lua
@@ -257,6 +257,60 @@ require('gitsigns').setup {
 				gitsigns.nav_hunk('prev')
 			end
 		end)
+
+		-- Navigation
+		map('n', ']c', function()
+			if vim.wo.diff then
+				vim.cmd.normal({']c', bang = true})
+			else
+				gitsigns.nav_hunk('next')
+			end
+		end)
+
+		map('n', '[c', function()
+			if vim.wo.diff then
+				vim.cmd.normal({'[c', bang = true})
+			else
+				gitsigns.nav_hunk('prev')
+			end
+		end)
+
+		-- Actions
+		map('n', '<leader>hs', gitsigns.stage_hunk, {desc="Stage Hunk"})
+		map('n', '<leader>hr', gitsigns.reset_hunk, {desc="Reset Hunk"})
+
+		map('v', '<leader>hs', function()
+			gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+		end, {desc="Stage Hunk"})
+
+		map('v', '<leader>hr', function()
+			gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+		end, {desc="Reset Hunk"})
+
+		map('n', '<leader>hS', gitsigns.stage_buffer, {desc="Stage Buffer"})
+		map('n', '<leader>hR', gitsigns.reset_buffer, {desc="Reset Buffer"})
+		map('n', '<leader>hp', gitsigns.preview_hunk, {desc="Preview Hunk"})
+		map('n', '<leader>hi', gitsigns.preview_hunk_inline, {desc="Preview Hunk (inline)"})
+
+		map('n', '<leader>hb', function()
+			gitsigns.blame_line({ full = true })
+		end, {desc="Blame line"})
+
+		map('n', '<leader>hd', gitsigns.diffthis, {desc=""})
+
+		map('n', '<leader>hD', function()
+			gitsigns.diffthis('~')
+		end, {desc=""})
+
+		map('n', '<leader>hQ', function() gitsigns.setqflist('all') end, {desc=""})
+		map('n', '<leader>hq', gitsigns.setqflist, {desc=""})
+
+		-- Toggles
+		map('n', '<leader>tb', gitsigns.toggle_current_line_blame, {desc=""})
+		map('n', '<leader>tw', gitsigns.toggle_word_diff, {desc=""})
+
+		-- Text object
+		map({'o', 'x'}, 'ih', gitsigns.select_hunk, {desc=""})
 	end
 }
 
